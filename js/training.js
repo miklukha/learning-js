1b52c05349705fefb63dda2a57d5e10d3b489925
Anna Miklukha <miklukha.anna@ukr.net>
2022-02-13 15:08:32 +0200

    add feature

--- js/training.js
+++ js/training.js
@@ -1,10 +1,8 @@
-const value = 27.5;
-console.log(Math.floor(value)); // 27
-console.log(Math.ceil(value)); // 28
-console.log(Math.round(value)); // 28
-console.log(Math.trunc(value)); // 27
-console.log(Math.min(23, 34, 7865, -123452, 5)); // -123452
-console.log(Math.max(23, 34, 7865, -123452, 5)); // 7865
-console.log(Math.pow(4, 2)); // 16
-console.log(4 ** 2); // 16
-console.log(Math.round(Math.random() * (68 - 1) + 1)); // a random number from 1 to 68
+let name = 'Lydia';
+
+function getName() {
+  console.log(name);
+  let name = 'Sarah';
+}
+
+getName(); // ReferenceError: Cannot access 'name' before initialization
---
+++ js/windowProperty.js
@@ -0,0 +1,20 @@
+(() => {
+  let x = (y = 10);
+})();
+
+console.log(typeof x); // undefined
+console.log(typeof y); // number
+console.log(window.y); // 10
+console.log(y); // 10
+
+c = 5;
+
+console.log(window.c); // 5
+console.log(c); // 5
+
+const a = (b = 5);
+
+console.log(a); // 5
+console.log(b); // 5
+console.log(typeof a); // number
+console.log(typeof b); // number
